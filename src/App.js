71e6276668fdd35aada17d2d51e50315e0b95db7
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-01-20 20:27:35 +0545

    simple context example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
-
+import { useContext } from "react";
+import MessageContext from "./context/MessageContext";
 function App() {
+  const message = "Hello World";
   return (
     <div className="App">
+      <MessageContext.Provider value={message}>
+        <ComponentA />
+      </MessageContext.Provider>
       <header className="App-header"></header>
     </div>
   );
 }
 
 export default App;
+
+const ComponentA = () => {
+  return (
+    <div>
+      <ComponentB />
+    </div>
+  );
+};
+
+const ComponentB = () => {
+  const message = useContext(MessageContext);
+  return (
+    <div>
+      <h1>Message: {message}</h1>
+    </div>
+  );
+};
